5f069eba48b6302fae2b9939d294e99fe61452de
CrazyEndery <113983929+CrazyEndery@users.noreply.github.com>
2022-10-18 22:24:58 +0300

    Update 1678a.cpp

--- 1678a.cpp
+++ 1678a.cpp
@@ -6,13 +6,15 @@ int main() {
 	for (int it = 0; it < t; it += 1) {
 		int n = 0;
 		std::cin >> n;
-		std::vector<int> N (n);
+		std::vector<int> N(n);
 		bool zero = false;
 		bool two_same = false;
+		int kz = 0;
 		for (int in = 0; in < n; in += 1) {
 			std::cin >> N[in];
 			if (N[in] == 0) {
 				zero = true;
+				kz += 1;
 			}
 		}
 		for (int i = 0; i < n - 1; i += 1) {
@@ -27,13 +29,13 @@ int main() {
 			}
 		}
 		if (zero) {
-			std::cout << n - 1 << std::endl;
+			std::cout << n - kz << std::endl;
 		}
 		else if (two_same) {
-			std::cout << n << std::endl;
-		} 
+			std::cout << n - kz + 1 << std::endl;
+		}
 		else {
-			std::cout << n + 1 << std::endl;
+			std::cout << n - kz + 2 << std::endl;
 		}
 	}
 }
